c2f265e7dd5e494e9b97fc8f04c6b3704a86551a
Michael Richardson <DragonRider0o0@gmail.com>
2014-10-26 09:09:10 -0400

    Refined LUA script to control playback and volume

--- Development/MYO Scripts/MYO.lua
+++ Development/MYO Scripts/MYO.lua
@@ -5,7 +5,7 @@ sensedRoll = 0
 currentYaw = 0
 currentPitch = 0
 currentRoll = 0
-sensitivity = 0.1
+sensitivity = 0.05
 enabled = true
 beginTime = myo.getTimeMilliseconds()
 currentTime = (myo.getTimeMilliseconds() - beginTime) / 1000
@@ -15,7 +15,20 @@ currentBeat = 4
 beatCount = 1
 secondsPerBeat = 0.714
 
+upCount = 0
+downCount = 0
+leftCount = 0
+rightCount = 0
+
 currentApp = ""
+
+minVolume = 0.5
+maxVolume = 1.0
+currentVolume = 0.5
+volumeIncrement = 0.1
+downIncrement = - 0.1
+
+notches = 0;
 
 
 
@@ -25,14 +38,16 @@ tempo = 84
 
 function onPoseEdge(pose, edge)
 	if currentApp == "AIRCHESTRATE" then
-		myo.debug("Time: " .. currentTime)
+		-- myo.debug("Time: " .. currentTime)
 		if pose == "fist" and edge == "on" then
 			-- myo.mouse("left", "down")
+			myo.keyboard("p", "press")
 			myo.debug("Fist")
 		end
 		if pose == "fist" and edge == "off"  then
 			-- myo.mouse("left", "up")
-			myo.debug("Unfist")
+			myo.keyboard("space", "press")
+			-- myo.debug("Unfist")
 		end
 
 		if pose == "thumbToPinky" then
@@ -47,14 +62,66 @@ function onPeriodic()
 	if expectedTime < currentTime then
 		beatCount = beatCount + 1;
 		currentBeat = (beatCount % 4) + 1
+		myo.debug("\tUp: " .. upCount .. "\tDown: " .. downCount .. "\tLeft: " .. leftCount .. "\tRight: " .. rightCount )
+
+		
+		
+		if currentBeat == 1 then
+			myo.vibrate("medium")
+			if downCount < 3 then 
+				downCount = 3
+			end
+			if downCount > 13 then 
+				downCount = 13
+			end
+			notches = math.floor((downCount - 3) / 2)
+			
+		end
+		if currentBeat == 2 then
+			myo.vibrate("short")
+			notches = math.floor((leftCount - 3) / 2)
+		end
+		if currentBeat == 3 then
+			myo.vibrate("short")
+			if rightCount < 3 then 
+				rightCount = 3
+			end
+			if rightCount > 13 then 
+				rightCount = 13
+			end
+			notches = math.floor((rightCount - 3) / 2)
+		end
+		if currentBeat == 4 then
+			myo.vibrate("short")
+			if upCount < 3 then 
+				upCount = 3
+			end
+			if upCount > 13 then 
+				upCount = 13
+			end
+			notches = math.floor((upCount - 3) / 2)
+		end
+		
+		upCount = 0
+		downCount = 0
+		leftCount = 0
+		rightCount = 0
+		
+		while currentVolume > minVolume do
+			currentVolume = currentVolume - volumeIncrement
+			myo.debug("Current Volume: " .. currentVolume)
+			myo.keyboard("down_arrow", "press")
+		end
+		while currentVolume < (minVolume + (notches * volumeIncrement)) do
+			myo.debug("Current Volume: " .. currentVolume)
+			currentVolume = currentVolume + volumeIncrement
+			myo.keyboard("up_arrow", "press")
+		end
+		
+		-- myo.debug("\t" .. notches)
 		
 		myo.debug("Beat: " .. currentBeat)
 		expectedTime = beatCount * secondsPerBeat;
-		if currentBeat == 1 then
-			myo.vibrate("medium")
-		else
-			myo.vibrate("short")
-		end
 	end
 		
 	sensedYaw = myo.getYaw()
@@ -62,18 +129,23 @@ function onPeriodic()
 	sensedRoll = myo.getRoll()
 	if currentYaw <= (sensedYaw - sensitivity) then
 		currentYaw = sensedYaw
+		
+		leftCount = leftCount + 1
 		-- myo.debug("Yaw: " .. currentYaw)
 	end
 	if currentYaw >= (sensedYaw + sensitivity) then
 		currentYaw = sensedYaw
+		rightCount = rightCount + 1
 		-- myo.debug("Yaw: " .. currentYaw)
 	end
 	if currentPitch <= (sensedPitch - sensitivity) then
 		currentPitch = sensedPitch
+		downCount = downCount + 1
 		-- myo.debug("Pitch: " .. currentPitch)
 	end
 	if currentPitch >= (sensedPitch + sensitivity) then
 		currentPitch = sensedPitch
+		upCount = upCount + 1
 		-- myo.debug("Pitch: " .. currentPitch)
 	end
 	if currentRoll <= (sensedRoll - sensitivity) then
@@ -88,7 +160,7 @@ function onPeriodic()
 end
 
 function onForegroundWindowChange(app, title)
-    myo.debug("onForegroundWindowChange: " .. app .. ", " .. title)
+    -- myo.debug("onForegroundWindowChange: " .. app .. ", " .. title)
 	currentApp = title
     return true
 end
@@ -98,5 +170,5 @@ function activeAppName()
 end
 
 function onActiveChange(isActive)
-    myo.debug("onActiveChange")
+    -- myo.debug("onActiveChange")
 end
