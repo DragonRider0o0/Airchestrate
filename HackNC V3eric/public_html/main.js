3caa7cd1beb39af72ed064fb69273c8c3047c075
ERIC YI <eyi@andrew.cmu.edu>
2014-10-26 05:37:23 -0400

    eric

---
+++ HackNC V3eric/public_html/main.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+setTimeout(function() {
+    console.log('fuck yeh');
+    document.getElementById("placeCircle").innerHTML="<img id='twinCircle' src='circle_light_green.svg' />";
+}, 0.001);
+ 
+window.addEventListener("mousedown", alertInfo);
+*/
+function alertInfo() {
+    var x = event.clientX;
+    var y = event.clientY;
+    console.log('('+x+','+y+')');
+}
