2e9868000be59ea5d2c1d6753938b26992dca168
Michael Richardson <DragonRider0o0@gmail.com>
2014-10-26 07:53:00 -0400

    Gave up kinetic scrolling and switched to listview

--- Development/Windows Modern App/ARCHESTRATE/bin/Debug/AppX/js/default.js
+++ Development/Windows Modern App/ARCHESTRATE/bin/Debug/AppX/js/default.js
@@ -31,22 +31,73 @@
     app.start();
 })();
 
-
-(function ()
-{
+(function () {
     "use strict";
 
-    var basicList = new WinJS.Binding.List(
-        [
-            { title: "Banana blast"},
-            { title: "Strawberry swirl"},
-            { title: "Magnificant mint"},
-            { title: "Lemon lift"}
-        ]);
+    var dataArray = [
+    { title: "America the Beautiful" },
+    { title: "Auld Lang Syne" },
+    { title: "Blowin in the Wind" },
+    { title: "Don't Think Twice, It's Alright" },
+    { title: "Camptown Races" },
+    { title: "Goodnight Irene" },
+    { title: "Home on the Range" },
+    { title: "We Shall Not Be Moved" },
+    { title:  "If I Had a Hammer" },
+    { title: "I've Been Working on the Railroad" },
+    { title: "Jingle Bells" },
+    { title: "Keep Your Eyes on the Prize" },
+    { title: "Like a Rolling Stone" },
+    { title: "Little Brown Jug" },
+    { title: "Michael Row the Boat Ashore" },
+    { title: "Love Me, I'm a Liberal" },
+    { title: "My Country Tis of Thee" },
+    { title: "O Christmas Tree" },
+    { title: "Oh Freedom" },
+    { title: "Oh Susanna" },
+    { title: "Peace Train" },
+    { title: "Rudolph the Red Nosed Reindeer" },
+    { title: "Scarborough Fair" },
+    { title: "Silent Night" },
+    { title: "Star Spangled Banner" },
+    { title: "Sunday Morning Coming Down" },
+    { title: "Swing Low Sweet Chariot" },
+    { title: "Take Me Out to the Ball Game" },
+    { title: "This Land is Your Land" },
+    { title: "This Little Light of Mine" },
+    { title: "Up to the Mountain" },
+    { title: "We Shall Overcome" },
+    { title: "Yankee Doodle" }
+    ];
 
-    WinJS.Namespace.define("ItemContainerExample",
+    var itemList = new WinJS.Binding.List(dataArray);
+
+    // Create a namespace to make the data publicly
+    // accessible. 
+    var publicMembers =
         {
-            flavorList: basicList
+            itemList: itemList
+        };
+    WinJS.Namespace.define("DataExample", publicMembers);
 
-        });
 })();
+
+
+//(function ()
+//{
+//    "use strict";
+
+//    var basicList = new WinJS.Binding.FlipView(
+//        [
+//            { title: "Banana blast"},
+//            { title: "Strawberry swirl"},
+//            { title: "Magnificant mint"},
+//            { title: "Lemon lift"}
+//        ]);
+
+//    WinJS.Namespace.define("ItemContainerExample",
+//        {
+//            flavorList: basicList
+
+//        });
+//})();
